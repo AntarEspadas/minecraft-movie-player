c6c49abf99506ca1dad2fbf918db91732235cd54
Naratna <59068049+Naratna@users.noreply.github.com>
2020-09-10 08:56:29 -0500

    Added skeleton for implementing behaviour Added output file argument to the video arguments

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ parser = argparse.ArgumentParser()
 parsers = parser.add_subparsers(help="commands",dest="command")
 
 video_parser = parsers.add_parser("video", help="convert video into blocks or maps")
-video_parser.add_argument("PATH_TO_VIDEO_FILE", type=str, help="The path to the video file that will be converted to maps or blocks")
+video_parser.add_argument("path_to_input_file", type=str, help="The path to the video file that will be converted to maps or blocks")
+video_parser.add_argument("path_to_output_file", type=str, help="The path to which the zip file containing the frames will be written")
 video_parser.add_argument("-f", "--ticks-per-frame", default="2", type=int, dest="ticks_per_frame",help="How many game ticks between every frame. Can be any integer from 1 to 20. Default is 2. 20 = 1 fps, 2 = 10 fps, 1 = 20 fps, etc...")
 video_parser.add_argument("-x", "--width", default=-1, type=int, dest="width", help="The width of the output video, measured in blocks or maps")
 video_parser.add_argument("-y", "--height",default=-1, type=int, dest="height", help="The height of the output video, measured in blocks or maps")
@@ -13,7 +14,7 @@ type_group = video_parser.add_mutually_exclusive_group(required = True)
 type_group.add_argument("-m","--maps", action="store_const", const="item-frame", dest="type", help="Tells the program to convert the video into maps")
 type_group.add_argument("-b","--blocks", action="store_const", const="blocks", dest="type", help="Tells the program to convert the video into blocks")
 
-audio_parser = parsers.add_parser("audio", help="splits audio into chunks")
+audio_parser = parsers.add_parser("audio", help="generates resourcepack containint audio files")
 audio_parser.add_argument("-s", "--split-size", default="1", type=int, dest="split_size")
 
 datapack_parser = parsers.add_parser("datapack", help="generates movie player datapack")
@@ -22,4 +23,11 @@ datapack_parser.add_argument("-m","--merge", action="store_true", dest="merge")
 #parser.add_argument("-nv", "--no-video", help="doesn't convert the video to any format", action="store_true")
 args = parser.parse_args()
 print(args.command)
+
+if args.command == "video":
+    pass
+elif args.command == "audio":
+    pass
+elif args.command == "datapack":
+    pass
 
