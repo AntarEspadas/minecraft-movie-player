3f7f7e36804543de7a6ac593fcfd9276c6877104
Naratna <59068049+Naratna@users.noreply.github.com>
2020-09-19 01:51:47 -0500

    Most functionallity implemented

--- image_converter.py
+++ image_converter.py
@@ -4,28 +4,50 @@ import structure_block
 import os
 import csv
 import kdtree
+import time
 
-def video_to_structure(path_to_video: str, destination_folder: str, name_prefix: str, path_to_palette: str = None, starting_number: int = None, width: int = None, height: int = None, adjust_mode: str = "fit"):
+def video_to_structure(path_to_video: str, destination_folder: str, name_prefix: str, path_to_palette: str = None, ticks_per_frame:int = 2, starting_frame: int = 0,  starting_number: int = None, width: int = None, height: int = None, adjust_mode: str = None):
     video = cv2.VideoCapture(path_to_video)
-    palette = __get_palette(path_to_palette or os.path.join(os.path.dirname(__name__),"palette.txt"))
-    success, image = video.read()
-    success, image = video.read()
-    success, image = video.read()
-    success, image = video.read()
-    success, image = video.read()
-    success, image = video.read()
-    success, image = video.read()
-    success, image = video.read()
+    palette = _get_palette(path_to_palette or os.path.join(os.path.dirname(__name__),"palette.txt"))
+    
+    starting_number = starting_number or starting_frame
+
+    output_fps = 20 / ticks_per_frame
+    video_fps = video.get(cv2.CAP_PROP_FPS)
+    frame_skip = video_fps / output_fps
+
+    video.set(cv2.CAP_PROP_POS_FRAMES, int(starting_frame * frame_skip) - 1)
     success, image = video.read()
     
+    size = _get_image_size(image, width, height, adjust_mode)
+    adjust = _get_adjuster(image, width, height, adjust_mode)
 
-    array_to_structure(image, palette).save(os.path.join(destination_folder,name_prefix + ".nbt"))
+    count = 0
+    while success:
+        start = time.time()
+        image = adjust(image, size)
+        array_to_structure(image, palette).save(os.path.join(destination_folder, f"{name_prefix}{starting_number + count}.nbt"))
+        print(f"{count} done in {time.time() - start} seconds")
+        count += 1
+        video.set(cv2.CAP_PROP_POS_FRAMES, int(frame_skip * (starting_frame + count)) - 1)
+        success, image = video.read()
 
-def image_to_structure(path_to_image: str, out_structure: str, path_to_palette: str = "palette.txt"):
-    palette = __get_palette(path_to_palette)
+def image_to_structure(path_to_image: str, out_structure: str, path_to_palette: str = "palette.txt", width: int = None, height: int = None, adjust_mode = None):
+    start = time.time()
+    palette = _get_palette(path_to_palette)
     image = cv2.imread(path_to_image)
+
+    size = _get_image_size(image, width, height, adjust_mode)
+    image = _get_adjuster(image, width, height, adjust_mode)(image, size)
+    print(time.time() - start)
+
+    start = time.time()
     structure = array_to_structure(image, palette)
+    print(time.time() - start)
+
+    start = time.time()
     structure.save(out_structure)
+    print(time.time() - start)
 
 def array_to_structure(image: numpy.ndarray, palette):
     structure = structure_block.StructureBlock((len(image[0]), len(image), 1))
@@ -37,7 +59,81 @@ def array_to_structure(image: numpy.ndarray, palette):
 
     return structure
 
-def __get_palette(path_to_palette):
+def _get_adjuster(image: numpy.ndarray, width: str = None, height: str = None, adjust_mode: str = None):
+    
+    def nop(image: numpy.ndarray, size: tuple) -> numpy.ndarray:
+        return image
+    
+    def resize_only(image: numpy.ndarray, size: tuple) -> numpy.ndarray:
+        return cv2.resize(image, size)
+
+    def resize_and_crop(image: numpy.ndarray, size: tuple) -> numpy.ndarray:
+        image = cv2.resize(image, size)
+        return _crop(image, width, height)
+
+    def resize_and_pad(image: numpy.ndarray, size: tuple) -> numpy.ndarray:
+        image = cv2.resize(image, size)
+        return _pad(image, width, height)
+
+    img_width = image.shape[1]
+    img_height = image.shape[0]
+
+    if width is None and height is None:
+        return nop
+
+    if width is None or height is None:
+        if width == img_width or height == img_height:
+            return nop
+        else:
+            return resize_only
+    
+    if (img_width, img_height) == (width, height):
+        return nop
+
+    if abs(img_width/ img_height - width / height) < 0.01:
+        return resize_only
+    
+    if adjust_mode == "fill":
+        return resize_and_crop
+
+    return resize_and_pad
+
+def _crop(image: numpy.ndarray, width: int, height: int):
+    img_width = image.shape[1]
+    img_height = image.shape[0]
+    w_offset = (img_width - width) // 2
+    h_offset = (img_height - height) // 2
+    return image[h_offset:h_offset + height, w_offset:w_offset + width]
+
+def _pad(image: numpy.ndarray, width: int, height: int):
+    img_width = image.shape[1]
+    img_height = image.shape[0]
+    w_offset = (width - img_width) // 2
+    h_offset = (height - img_height) // 2
+    destination_image = numpy.zeros((height, width, 3))
+    destination_image[h_offset:h_offset + img_height, w_offset: w_offset + img_width] = image
+    return destination_image
+
+def _get_image_size(image: numpy.ndarray, width: int = None, height: int = None, adjust_mode: str = None):
+    img_width = image.shape[1]
+    img_height = image.shape[0]
+    
+    if width is not None and height is not None:
+        dif = img_width / img_height > width / height
+        if dif and adjust_mode != "fill":
+            height = None
+        else:
+            width = None
+
+    if width is None and height is None:
+        return img_width, img_height
+    if height is None:
+        height = img_height / img_width * width
+    elif width is None:
+        width = img_width / img_height * height
+    return int(width), int(height)
+
+def _get_palette(path_to_palette):
     palette_file = open(path_to_palette,newline="")
     palette_reader = csv.reader(palette_file,delimiter=",",quotechar="|")
     blocks = [_Block(tuple([int(i) for i in block[1:4]]),block[0], block[4] or None) for block in palette_reader]
@@ -59,12 +155,15 @@ class _Block:
 
 
 if __name__ == "__main__":
-    path_to_video = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\rickroll.mp4"#"D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\bbb_sunflower_1080p_30fps_normal.mp4"
-    output_folder = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io"
+    #path_to_video = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\rickroll_cut.mp4"
+    path_to_video = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\bbb_sunflower_1080p_30fps_normal.mp4"
+    output_folder = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\big_buck_bunny"
     palette = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\palette.txt"
-    path_to_image = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\carved_pumpkin.png"
-    path_to_structure = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\carved_pumpkin.nbt"
-    name_prefix = "test"
-    image_to_structure(path_to_image, path_to_structure, palette)
-    #video_to_structure(path_to_video,output_folder,name_prefix,palette)
+    #path_to_image = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\carved_pumpkin.png"
+    path_to_image = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\test.jpg"
+    #path_to_structure = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\carved_pumpkin.nbt"
+    path_to_structure = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\test.nbt"
+    name_prefix = "big_buck_bunny_"
+    #image_to_structure(path_to_image, path_to_structure, palette, 75)
+    video_to_structure(path_to_video, output_folder, name_prefix, path_to_palette= palette, width= 50, starting_frame=200)
     #__get_palette(palette)
--- tests/image_converter_test.py
+++ tests/image_converter_test.py
@@ -1,10 +1,84 @@
 import unittest
+import numpy
+import cv2
+import os
 from context import image_converter
 
 class ImageConverterTests(unittest.TestCase):
 
     def test_get_tree(self):
-        image_converter.__get_palette("D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\palette.txt")
+        image_converter._get_palette("D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\palette.txt")
+    
+    def test_resize(self):
+        image = numpy.zeros((90,160))
+        size = image_converter._get_image_size(image, width = 80)
+        self.assertTupleEqual(size, (80,45))
+
+        size = image_converter._get_image_size(image, height = 45)
+        self.assertTupleEqual(size, (80,45))
+
+        size = image_converter._get_image_size(image)
+        self.assertTupleEqual(size, (160, 90))
+
+        size = image_converter._get_image_size(image, width = 80, height= 45, adjust_mode = "fit")
+        self.assertTupleEqual(size,(80,45))
+
+        size = image_converter._get_image_size(image, width = 80, height= 45, adjust_mode = "fill")
+        self.assertTupleEqual(size,(80,45))
+
+        size = image_converter._get_image_size(image, width = 80, height= 100, adjust_mode = "fit")
+        self.assertTupleEqual(size,(80,45))
+
+        size = image_converter._get_image_size(image, width = 80, height= 100, adjust_mode = "fill")
+        self.assertTupleEqual(size,(177,100))
+
+    def test_get_adjuster(self):
+        image = numpy.zeros((90,160))
+
+        adjuster = image_converter._get_adjuster(image)
+        self.assertEqual(adjuster.__name__, "nop")
+
+        adjuster = image_converter._get_adjuster(image, 160, 90)
+        self.assertEqual(adjuster.__name__, "nop")
+
+        adjuster = image_converter._get_adjuster(image, 80, 45)
+        self.assertEqual(adjuster.__name__, "resize_only")
+
+        adjuster = image_converter._get_adjuster(image, 80, 100, adjust_mode = "fit")
+        self.assertEqual(adjuster.__name__, "resize_and_pad")
+
+        adjuster = image_converter._get_adjuster(image, 80, 100, adjust_mode = "fill")
+        self.assertEqual(adjuster.__name__, "resize_and_crop")
+
+    def test_adjust(self):
+        folder = os.path.dirname(__file__)
+        io_folder = os.path.join(folder, "..", "test-io")
+        image_path = os.path.join(io_folder, "test.jpg")
+        print(image_path)
+        image = cv2.imread(image_path)
+
+        self.assertIsNotNone(image)
+
+        size = image_converter._get_image_size(image, width = 100)
+        adjust = image_converter._get_adjuster(image, width = 100)
+        self.assertEqual(adjust.__name__, "resize_only")
+        cv2.imwrite(os.path.join(io_folder, "test_adjust_1.png"), adjust(image, size))
+
+        size = image_converter._get_image_size(image, height = 100)
+        adjust = image_converter._get_adjuster(image, height = 100)
+        self.assertEqual(adjust.__name__, "resize_only")
+        cv2.imwrite(os.path.join(io_folder, "test_adjust_2.png"), adjust(image, size))
+
+        size = image_converter._get_image_size(image, 100, 100, "fit")
+        adjust = image_converter._get_adjuster(image, 100, 100, "fit")
+        self.assertEqual(adjust.__name__, "resize_and_pad")
+        cv2.imwrite(os.path.join(io_folder, "test_adjust_3.png"), adjust(image, size))
+
+        size = image_converter._get_image_size(image, 100, 100, "fill")
+        adjust = image_converter._get_adjuster(image, 100, 100, "fill")
+        self.assertEqual(adjust.__name__, "resize_and_crop")
+        cv2.imwrite(os.path.join(io_folder, "test_adjust_4.png"), adjust(image, size))
 
 if __name__ == "__main__":
     unittest.main()
+    #print(image_converter._get_palette)
