746ba30c4fb94c4694564c202cb2ebc25770639a
Naratna <59068049+Naratna@users.noreply.github.com>
2020-09-22 06:31:21 -0500

    Can now generate sound functions

--- player_datapack.py
+++ player_datapack.py
@@ -13,14 +13,14 @@ def generate_structure_functions(output_folder: str, datapack_name:str, filename
             first_index = file_number * step * max_commands
             last_index = (file_number + 1) * step * max_commands - 1
             if power == 0:
-                _write_primary_function(output_folder, datapack_name, filename_prefix, first_index, min(last_index, final_index), file_amount == 1)
+                _write_primary_video_function(output_folder, datapack_name, filename_prefix, first_index, min(last_index, final_index), file_amount == 1)
             else:
-                _write_secondary_function(output_folder, datapack_name, first_index, min(last_index, final_index), step, file_amount == 1)
+                _write_secondary_video_function(output_folder, datapack_name, first_index, min(last_index, final_index), step, file_amount == 1)
 
     _write_setup_video_function(output_folder, datapack_name)
     _write_main_video_function(output_folder, datapack_name, ticks_per_frame)
 
-def generate_audio_functions(output_folder: str, datapack_name: str, sound_name: str, sound_duration: str, first_index: int, final_index: int, max_commands: int = 25):
+def generate_audio_functions(output_folder: str, datapack_name: str, sound_name_prefix: str, sound_duration: float, first_index: int, final_index: int, max_commands: int = 25):
     
     layers = _calculate_layers(first_index, final_index, max_commands)
 
@@ -29,6 +29,43 @@ def generate_audio_functions(output_folder: str, datapack_name: str, sound_name:
         for file_number in range(file_amount):
             first_index = file_number * step * max_commands
             last_index = (file_number + 1) * step * max_commands - 1
+            last_index = min(last_index, final_index)
+            if power == 0:
+                _write_primary_audio_function(output_folder, datapack_name, sound_name_prefix, first_index, last_index)
+            else:
+                _write_secondary_audio_function(output_folder, datapack_name, first_index, last_index, step, file_amount == 1)
+    
+    _write_setup_audio_function(output_folder, datapack_name)
+    _write_main_audio_function(output_folder, datapack_name, sound_duration)
+
+
+
+def _write_setup_audio_function(output_folder: str, datapack_name: str):
+    function = open(os.path.join(output_folder, "setup_audio.mcfunction"), "w+")
+    
+    commands = []
+
+    commands += f"scoreboard objectives add {datapack_name}_am dummy",
+    commands += f"scoreboard objectives add {datapack_name}_at dummy",
+
+    function.write("\n".join(commands))
+    function.close()
+
+def _write_main_audio_function(output_folder: str, datapack_name: str, sound_duration: int):
+    function = open(os.path.join(output_folder, "main_audio.mcfunction"), "w+")
+
+    ticks = sound_duration * 20
+
+    commands = []
+
+    commands += 'scoreboard players add @e[type=armor_stand, name=%s_screen, scores={%s_am=0..}] %s_am 1' % ((datapack_name,) * 3),
+    commands += 'scoreboard players set @e[type=armor_stand, name=%s_screen, scores={%s_am=%d..}] %s_am 0' % (datapack_name, datapack_name, ticks, datapack_name),
+    commands += 'scoreboard players add @e[type=armor_stand, name=%s_screen, scores={%s_am=0}] %s_at 1' % ((datapack_name,) * 3),
+    commands += 'execute at @e[type=armor_stand, name=%s_screen, scores={%s_am=0}] run function %s:audio_root' % ((datapack_name,) * 3),
+
+    function.writelines("\n".join(commands))
+    function.close()
+
 
 def _write_setup_video_function(output_folder: str, datapack_name: str):
     function = open(os.path.join(output_folder, "setup_video.mcfunction"), "w+")
@@ -43,49 +80,77 @@ def _write_setup_video_function(output_folder: str, datapack_name: str):
     function.write("\n".join(commands))
     function.close()
 
-def _write_main_video_function(output_folder: str, datapack_name: str, ticks_per_frame: int, with_audio = False):
+def _write_main_video_function(output_folder: str, datapack_name: str, ticks_per_frame: int):
     function = open(os.path.join(output_folder, "main_video.mcfunction"), "w+")
 
     commands = []
 
-    commands += f'scoreboard players add @e[type=armor_stand, name={datapack_name}_screen] {datapack_name}_vm 1',
+    commands += 'scoreboard players add @e[type=armor_stand, name=%s_screen, scores={%s_vm=0..}] %s_vm 1' % ((datapack_name,) * 3),
     commands += 'scoreboard players set @e[type=armor_stand, name=%s_screen, scores={%s_vm=%d..}] %s_vm 0' % (datapack_name, datapack_name, ticks_per_frame, datapack_name),
     commands += 'scoreboard players add @e[type=armor_stand, name=%s_screen, scores={%s_vm=0}] %s_vt 1' % ((datapack_name,) * 3),
-    commands += f'function {datapack_name}:video_root',
-    commands += 'execute at @e[type=armor_stand, name=%s_screen, scores={%s_vt=0..}] run setblock ~ ~ ~ stone' % ((datapack_name,) * 2),
-    commands += 'execute at @e[type=armor_stand, name=%s_screen, scores={%s_vt=0..}] run setblock ~ ~ ~ redstone_block' % ((datapack_name,) * 2),
+    commands += 'execute at @e[type=armor_stand, name=%s_screen, scores={%s_vm=0}] run function %s:video_root' % ((datapack_name,) * 3),
+    commands += 'execute at @e[type=armor_stand, name=%s_screen, scores={%s_vm=0}] run setblock ~ ~ ~ stone' % ((datapack_name,) * 2),
+    commands += 'execute at @e[type=armor_stand, name=%s_screen, scores={%s_vm=0}] run setblock ~ ~ ~ redstone_block' % ((datapack_name,) * 2),
 
     function.writelines("\n".join(commands))
     function.close()
 
-def _write_primary_function(folder: str, datapack_name: str, filename_prefix: str, first_index: int, last_index: int, is_root = False, score_suffix: str = None, command: str = None):
+
+
+def _write_primary_audio_function(folder: str, datapack_name: str, sound_name_prefix: str, first_index: int, last_index: int, is_root = False):
+    filename = "audio_root.mcfunction" if is_root else f"audio_{first_index}-{last_index}.mcfunction"
+    function = open(os.path.join(folder, filename), "w+")
+    for i in range(first_index, last_index + 1):
+        function.write(_get_primary_audio_command(datapack_name, sound_name_prefix, i))
+    function.close()
+
+def _write_secondary_audio_function(folder: str, datapack_name: str, first_index: int, last_index: int, step: int, is_root = False):
+    filename = "audio_root.mcfunction" if is_root else f"audio_{first_index}-{last_index}.mcfunction"
+    function = open(os.path.join(folder, filename), "w+")
+    for i in range(first_index, last_index + 1, step):
+        function.write(_get_secondary_audio_command(datapack_name, i, min(i + step -1, last_index)))
+    function.close()
+
+def _write_primary_video_function(folder: str, datapack_name: str, filename_prefix: str, first_index: int, last_index: int, is_root = False):
     filename = "video_root.mcfunction" if is_root else f"video_{first_index}-{last_index}.mcfunction"
     function = open(os.path.join(folder, filename), "w+")
     for i in range(first_index, last_index + 1):
-        function.write(_get_primary_command(datapack_name, filename_prefix, i, score_suffix, command))
+        function.write(_get_primary_video_command(datapack_name, filename_prefix, i))
     function.close()
 
-def _write_secondary_function(folder: str, datapack_name: str, first_index: int, last_index: int, step: int, is_root = False):
+def _write_secondary_video_function(folder: str, datapack_name: str, first_index: int, last_index: int, step: int, is_root = False):
     filename = "video_root.mcfunction" if is_root else f"video_{first_index}-{last_index}.mcfunction"
     function = open(os.path.join(folder, filename), "w+")
     for i in range(first_index, last_index + 1, step):
-        function.write(_get_secondary_command(datapack_name, i, min(i + step -1, last_index)))
+        function.write(_get_secondary_video_command(datapack_name, i, min(i + step -1, last_index)))
     function.close()
 
 
-def _get_primary_command(datapack_name: str, filename_prefix: str, index: int, score_suffix: str = None, command: str = None):
-    score_suffix = score_suffix if score_suffix is not None else "vt"
+
+def _get_primary_audio_command(datapack_name: str, sound_name_prefix: str, index: int):
     execute = f'execute as @e[type = armor_stand, name="{datapack_name}_screen"] at @s'
-    condition = f'if score @s {datapack_name}_{score_suffix} matches {index}'
-    command = command if command is not None else 'run data merge block ~ ~-1 ~ {name:"%s:%s%s"}' % (datapack_name, filename_prefix, index)
-    command.replace("%d",str(index))
+    condition = f'if score @s {datapack_name}_at matches {index}'
+    command = f'run playsound {sound_name_prefix}{index} record @a ~ ~ ~'
     return f"{execute} {condition} {command}\n"
 
-def _get_secondary_command(datapack_name: str, first_index: int, last_index: int):
+def _get_secondary_audio_command(datapack_name: str, first_index: int, last_index: int):
+    execute = f'execute as @e[type=armor_stand, name="{datapack_name}_screen"]'
+    condition = f'if score @s {datapack_name}_at matches {first_index}..{last_index}'
+    command = f'run function {datapack_name}:audio_{first_index}-{last_index}'
+    return f"{execute} {condition} {command}\n"
+
+def _get_primary_video_command(datapack_name: str, filename_prefix: str, index: int):
+    execute = f'execute as @e[type = armor_stand, name="{datapack_name}_screen"] at @s'
+    condition = f'if score @s {datapack_name}_vt matches {index}'
+    command = 'run data merge block ~ ~-1 ~ {name:"%s:%s%s"}' % (datapack_name, filename_prefix, index)
+    return f"{execute} {condition} {command}\n"
+
+def _get_secondary_video_command(datapack_name: str, first_index: int, last_index: int):
     execute = f'execute as @e[type=armor_stand, name="{datapack_name}_screen"]'
     condition = f'if score @s {datapack_name}_vt matches {first_index}..{last_index}'
     command = f'run function {datapack_name}:video_{first_index}-{last_index}'
     return f"{execute} {condition} {command}\n"
+
 
 def _calculate_layers(first_index: int, final_index: int, max_commands: int) -> list:
     command_amount = final_index - first_index + 1
@@ -93,4 +158,5 @@ def _calculate_layers(first_index: int, final_index: int, max_commands: int) -> 
 
 if __name__ == "__main__":
     output_folder = "D:\\Desarrollo\\Python\\minecraft-movie-player\\test-io\\function_testing"
-    generate_structure_functions(output_folder,"testpack","rickroll_", 0, 847, 10, 5)
+    #generate_structure_functions(output_folder,"testpack","rickroll_", 0, 847, 10, 5)
+    generate_audio_functions(output_folder, "testpack", "testpack:rickroll_", 10, 0, 21, 10)
