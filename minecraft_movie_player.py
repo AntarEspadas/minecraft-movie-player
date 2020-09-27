41221a2c62966e296337e86f92fe729e7b875c64
Naratna <59068049+Naratna@users.noreply.github.com>
2020-09-27 05:32:36 -0500

    Fixed a couple bugs in the arguments

--- minecraft_movie_player.py
+++ minecraft_movie_player.py
@@ -8,10 +8,10 @@ def main():
     video_parser.add_argument("path_to_video", type=str, help="The path to the video file that will be converted to structure files")
     video_parser.add_argument("path_to_output_folder", type=str, help="The path to the folder in which the frames will be written")
     video_parser.add_argument("-p", "--palette", default="palette.txt", type=str, dest="path_to_palette",help="The path to the block palette file to be used when convertnig")
+    video_parser.add_argument("-n", "--name-prefix", default="video_", type=str, dest="name_prefix", help="Specifies the name that will be prefixed to every generated frame")
     video_parser.add_argument("-t", "--ticks-per-frame", default="2", type=int, dest="ticks_per_frame",help="How many game ticks between every frame. Can be any integer from 1 to 20. Default is 2. 20 = 1 fps, 2 = 10 fps, 1 = 20 fps, etc...")
     video_parser.add_argument("-x", "--width", default=-1, type=int, dest="width", help="The width of the output frames, measured in blocks")
     video_parser.add_argument("-y", "--height",default=-1, type=int, dest="height", help="The height of the output frames, measured in blocks")
-    video_parser.add_argument("-p", "--name-prefix", default="video_", type=str, dest="name_prefix", help="Specifies the name that will be prefixed to every generated frame")
     video_parser.add_argument("-u","--unoptimized", action="store_true", dest="unoptimized", help="By default, every frame contains only the blocks that differ from the previous frame in an attempt to save resources. Using this option will disable this feature")
 
 
@@ -46,7 +46,7 @@ def main():
     audio_functions_parser = functions_parsers.add_parser("audio", help="Generate the functions required for audio playbak")
     audio_functions_parser.add_argument("output_folder", type=str, help="The path to the folder in which the audio functions will be written")
     audio_functions_parser.add_argument("amount_of_sound_files", type=int, help="The amount of previously generated sound files")
-    audio_functions_parser.add_argument("-d","--datapack-name", type=str, default="player", dest="datapack_name", help="The name you wish to give to your datapack, should remain consistent across all functions and be less than 13 alphabetic lowercase characters")
+    audio_functions_parser.add_argument("-n","--datapack-name", type=str, default="player", dest="datapack_name", help="The name you wish to give to your datapack, should remain consistent across all functions and be less than 13 alphabetic lowercase characters")
     audio_functions_parser.add_argument("-p","--name-prefix", type=str, default="audio_", dest="name_prefix", help="The name prefix of the audio files that should have been generated beforehand")
     audio_functions_parser.add_argument("-d","--sound-duration", type=float, default=60, dest="sound_duration", help="The duration in seconds of the previously generated sound files")
     audio_functions_parser.add_argument("-m","--max-commands", type=int, default=25, dest="max_commands", help="The maximum amount of commands per generated function file, too many commands in a single function may impact performance. The less commands per function file, the more files will be generated")
@@ -55,7 +55,7 @@ def main():
     playback_control_functions_parser = functions_parsers.add_parser("playback-control", help="Generate the functions required for controlling the video and audio playback")
     playback_control_functions_parser.add_argument("output_folder", type=str, help="The path to the folder in which the playback control functions will be written")
     playback_control_functions_parser.add_argument("-d","--datapack-name", type=str, default="player", dest="datapack_name", help="The name you wish to give to your datapack, should remain consistent across all functions and be less than 13 alphabetic lowercase characters")
-    playback_control_functions_parser.add_argument("-a","--control-audio", action="store_true", default=False, type=bool, dest="control_audio", help="If present, the functions will be able to control the audio playback aswell, if the datapack uses no audio, enabling this option will break the playback control")
+    playback_control_functions_parser.add_argument("-a","--control-audio", action="store_true", default=False, dest="control_audio", help="If present, the functions will be able to control the audio playback aswell, if the datapack uses no audio, enabling this option will break the playback control")
 
     #parser.add_argument("-nv", "--no-video", help="doesn't convert the video to any format", action="store_true")
     args = parser.parse_args()
