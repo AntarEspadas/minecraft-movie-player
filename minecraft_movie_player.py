8538b4fa7783ed6331e2841222931a5cbc9a350c
Naratna <59068049+Naratna@users.noreply.github.com>
2020-09-27 13:26:46 -0500

    Added methods to validate the different types of inputs

--- minecraft_movie_player.py
+++ minecraft_movie_player.py
@@ -1,62 +1,82 @@
+
+import minecraft_movie_player_controller as controller
+import argparse
+
+def wrap(function):
+    def validate(value):
+        try:
+            return function(value)
+        except ValueError as err:
+            raise argparse.ArgumentTypeError(str(err))
+    return validate
+
+fold = wrap(controller.fold)
+fil = wrap(controller.fil)
+palette = wrap(controller.palette)
+vid = wrap(controller.vid)
+filename = wrap(controller.filename)
+mfilename = lambda max_chars: wrap(controller.mfilename(max_chars))
+mint = lambda minimum, nullable=False: wrap(controller.mint(minimum, nullable))
+mfloat = lambda minimum, nullable=False: wrap(controller.mfloat(minimum, nullable))
+
 def main():
-    import argparse
-    import minecraft_movie_player_controller as controller
 
     parser = argparse.ArgumentParser()
     parsers = parser.add_subparsers(help="commands", dest="command")
 
+
     video_parser = parsers.add_parser("video", help="convert video to structure files")
-    video_parser.add_argument("path_to_video", type=str, help="The path to the video file that will be converted to structure files")
-    video_parser.add_argument("path_to_output_folder", type=str, help="The path to the folder in which the frames will be written")
-    video_parser.add_argument("-p", "--palette", default="palette.txt", type=str, dest="path_to_palette",help="The path to the block palette file to be used when convertnig")
-    video_parser.add_argument("-n", "--name-prefix", default="video_", type=str, dest="name_prefix", help="Specifies the name that will be prefixed to every generated frame")
-    video_parser.add_argument("-t", "--ticks-per-frame", default="2", type=int, dest="ticks_per_frame",help="How many game ticks between every frame. Can be any integer from 1 to 20. Default is 2. 20 = 1 fps, 2 = 10 fps, 1 = 20 fps, etc...")
-    video_parser.add_argument("-x", "--width", default=None, type=int, dest="width", help="The width of the output frames, measured in blocks")
-    video_parser.add_argument("-y", "--height", default=None, type=int, dest="height", help="The height of the output frames, measured in blocks")
+    video_parser.add_argument("path_to_video", type=vid, help="The path to the video file that will be converted to structure files")
+    video_parser.add_argument("path_to_output_folder", type=fold, help="The path to the folder in which the frames will be written")
+    video_parser.add_argument("-p", "--palette", default="palette.txt", type=palette, dest="path_to_palette",help="The path to the block palette file to be used when convertnig")
+    video_parser.add_argument("-n", "--name-prefix", default="video_", type=filename, dest="name_prefix", help="Specifies the name that will be prefixed to every generated frame")
+    video_parser.add_argument("-t", "--ticks-per-frame", default="2", type=mint(0), dest="ticks_per_frame",help="How many game ticks between every frame. Can be any integer from 1 to 20. Default is 2. 20 = 1 fps, 2 = 10 fps, 1 = 20 fps, etc...")
+    video_parser.add_argument("-x", "--width", default=None, type=mint(0, True), dest="width", help="The width of the output frames, measured in blocks")
+    video_parser.add_argument("-y", "--height", default=None, type=mint(0, True), dest="height", help="The height of the output frames, measured in blocks")
     video_parser.add_argument("-m", "--adjust-mode", default="fit", type=str, dest="adjust_mode", choices=["fit", "fill"], help="If 'fit' is selected, the black bars will be added to make the image fit the specified with and height. If set to 'fill', the image will be cropped in order to fit the width and height specified. Only used if both with and height are specified")
     video_parser.add_argument("-u","--unoptimized", action="store_true", dest="unoptimized", help="By default, every frame contains only the blocks that differ from the previous frame in an attempt to save resources. Using this option will disable this feature")
-    video_parser.add_argument("-s", "--subprocesses", default=None, type=int, dest="subprocesses", help="The amount of subprocesses that should be used when generating the frames. Defaults to the amount of cores on your PC")
+    video_parser.add_argument("-s", "--subprocesses", default=None, type=mint(0, True), dest="subprocesses", help="The amount of subprocesses that should be used when generating the frames. Defaults to the amount of cores on your PC")
 
     resourcepak_parser = parsers.add_parser("resourcepack", help="Generate sound files and sounds.json")
     resourcepack_parsers = resourcepak_parser.add_subparsers(help="subcommands", dest="subcommand")
 
     resourcepack_audio_parser = resourcepack_parsers.add_parser("audio", help="Splits the audio track from a video or audio file into several vorbis encoded audio files so that it can be used by the player")
-    resourcepack_audio_parser.add_argument("path_to_audio", type=str, help="The path to the audio file that will be split and converted")
-    resourcepack_audio_parser.add_argument("path_to_output_folder", type=str, help="The path to the folder in which the audio files will be written")
-    resourcepack_audio_parser.add_argument("-s", "--split-size", default="60", type=float, dest="split_size", help="The duration in seconds of every generated audio file")
-    resourcepack_audio_parser.add_argument("-p", "--name-prefix", default="audio_", type=str, dest="name_prefix", help="Specifies the name that will be prefixed to every generated frame")
+    resourcepack_audio_parser.add_argument("path_to_audio", type=fil, help="The path to the audio file that will be split and converted")
+    resourcepack_audio_parser.add_argument("path_to_output_folder", type=fold, help="The path to the folder in which the audio files will be written")
+    resourcepack_audio_parser.add_argument("-s", "--split-size", default="60", type=mfloat(0), dest="split_size", help="The duration in seconds of every generated audio file")
+    resourcepack_audio_parser.add_argument("-p", "--name-prefix", default="audio_", type=filename, dest="name_prefix", help="Specifies the name that will be prefixed to every generated frame")
     
     resourcepack_json_parser = resourcepack_parsers.add_parser("json", help="Generates the sounds.json file required to create thr resourcepack")
-    resourcepack_json_parser.add_argument("output_folder", type=str, help="The path to the folder in which the sounds.json file will be written")
-    resourcepack_json_parser.add_argument("amount_of_sound_files", type=int, help="The amount of sound files that should be added to sonds.json")
-    resourcepack_json_parser.add_argument("-p", "--name-prefix", type=str, default="audio_", dest="name_prefix", help="The name that was prefixed to every sound file")
-    resourcepack_json_parser.add_argument("-s","--subfolder-name", default="player", dest="subfolder_name", type=str, help="The name of the resourcepack subfolder where the audio files should be moved to")
+    resourcepack_json_parser.add_argument("output_folder", type=fold, help="The path to the folder in which the sounds.json file will be written")
+    resourcepack_json_parser.add_argument("amount_of_sound_files", type=mint(0), help="The amount of sound files that should be added to sonds.json")
+    resourcepack_json_parser.add_argument("-p", "--name-prefix", type=filename, default="audio_", dest="name_prefix", help="The name that was prefixed to every sound file")
+    resourcepack_json_parser.add_argument("-s","--subfolder-name", type=filename, default="player", dest="subfolder_name", help="The name of the resourcepack subfolder where the audio files should be moved to")
 
     functions_parser = parsers.add_parser("functions", help="generates movie player functions")
     functions_parsers = functions_parser.add_subparsers(help="subcommands", dest="subcommand")
 
     #(output_folder: str, datapack_name:str, filename_prefix:str, first_index: int, final_index: int, max_commands: int = 25, ticks_per_frame: int = 2)
     video_functions_parser = functions_parsers.add_parser("video", help="Generate the functions required for video playbak")
-    video_functions_parser.add_argument("output_folder", type=str, help="The path to the folder in which the video functions will be written")
-    video_functions_parser.add_argument("amount_of_frames", type=int, help="The amount of frames that were generated previously")
-    video_functions_parser.add_argument("-d","--datapack-name", type=str, default="player", dest="datapack_name", help="The name you wish to give to your datapack, should remain consistent across all functions and be less than 13 alphabetic lowercase characters")
-    video_functions_parser.add_argument("-p","--name-prefix", type=str, default="video_", dest="name_prefix", help="the name prefix of the frames that should have been generated beforehand")
-    video_functions_parser.add_argument("-m","--max-commands", type=int, default=25, dest="max_commands", help="The maximum amount of commands per generated function file, too many commands in a single function may impact performance. The less commands per function file, the more files will be generated")
-    video_functions_parser.add_argument("-t", "--ticks-per-frame", type=int, default=2, dest="ticks_per_frame", help="How many game ticks between every frame. If the number is different that the one used to generate the frames, the video will be sped up or slowed down")
+    video_functions_parser.add_argument("output_folder", type=fold, help="The path to the folder in which the video functions will be written")
+    video_functions_parser.add_argument("amount_of_frames", type=mint(0), help="The amount of frames that were generated previously")
+    video_functions_parser.add_argument("-d","--datapack-name", type=mfilename(13), default="player", dest="datapack_name", help="The name you wish to give to your datapack, should remain consistent across all functions and be less than 13 alphabetic lowercase characters")
+    video_functions_parser.add_argument("-p","--name-prefix", type=filename, default="video_", dest="name_prefix", help="the name prefix of the frames that should have been generated beforehand")
+    video_functions_parser.add_argument("-m","--max-commands", type=mint(0), default=25, dest="max_commands", help="The maximum amount of commands per generated function file, too many commands in a single function may impact performance. The less commands per function file, the more files will be generated")
+    video_functions_parser.add_argument("-t", "--ticks-per-frame", type=mint(0), default=2, dest="ticks_per_frame", help="How many game ticks between every frame. If the number is different that the one used to generate the frames, the video will be sped up or slowed down")
 
     #(output_folder: str, datapack_name: str, sound_name_prefix: str, sound_duration: float, first_index: int, final_index: int, max_commands: int = 25)
     audio_functions_parser = functions_parsers.add_parser("audio", help="Generate the functions required for audio playbak")
-    audio_functions_parser.add_argument("output_folder", type=str, help="The path to the folder in which the audio functions will be written")
-    audio_functions_parser.add_argument("amount_of_sound_files", type=int, help="The amount of previously generated sound files")
-    audio_functions_parser.add_argument("-n","--datapack-name", type=str, default="player", dest="datapack_name", help="The name you wish to give to your datapack, should remain consistent across all functions and be less than 13 alphabetic lowercase characters")
-    audio_functions_parser.add_argument("-p","--name-prefix", type=str, default="audio_", dest="name_prefix", help="The name prefix of the audio files that should have been generated beforehand")
-    audio_functions_parser.add_argument("-d","--sound-duration", type=float, default=60, dest="sound_duration", help="The duration in seconds of the previously generated sound files")
-    audio_functions_parser.add_argument("-m","--max-commands", type=int, default=25, dest="max_commands", help="The maximum amount of commands per generated function file, too many commands in a single function may impact performance. The less commands per function file, the more files will be generated")
+    audio_functions_parser.add_argument("output_folder", type=fold, help="The path to the folder in which the audio functions will be written")
+    audio_functions_parser.add_argument("amount_of_sound_files", type=mint(0), help="The amount of previously generated sound files")
+    audio_functions_parser.add_argument("-n","--datapack-name", type=mfilename(13), default="player", dest="datapack_name", help="The name you wish to give to your datapack, should remain consistent across all functions and be less than 13 alphabetic lowercase characters")
+    audio_functions_parser.add_argument("-p","--name-prefix", type=filename, default="audio_", dest="name_prefix", help="The name prefix of the audio files that should have been generated beforehand")
+    audio_functions_parser.add_argument("-d","--sound-duration", type=mfloat(0), default=60, dest="sound_duration", help="The duration in seconds of the previously generated sound files")
+    audio_functions_parser.add_argument("-m","--max-commands", type=mint(0), default=25, dest="max_commands", help="The maximum amount of commands per generated function file, too many commands in a single function may impact performance. The less commands per function file, the more files will be generated")
 
     #(output_folder: str, datapack_name: str, control_audio: bool = False)
     playback_control_functions_parser = functions_parsers.add_parser("playback-control", help="Generate the functions required for controlling the video and audio playback")
-    playback_control_functions_parser.add_argument("output_folder", type=str, help="The path to the folder in which the playback control functions will be written")
-    playback_control_functions_parser.add_argument("-d","--datapack-name", type=str, default="player", dest="datapack_name", help="The name you wish to give to your datapack, should remain consistent across all functions and be less than 13 alphabetic lowercase characters")
+    playback_control_functions_parser.add_argument("output_folder", type=fold, help="The path to the folder in which the playback control functions will be written")
+    playback_control_functions_parser.add_argument("-d","--datapack-name", type=filename, default="player", dest="datapack_name", help="The name you wish to give to your datapack, should remain consistent across all functions and be less than 13 alphabetic lowercase characters")
     playback_control_functions_parser.add_argument("-a","--control-audio", action="store_true", default=False, dest="control_audio", help="If present, the functions will be able to control the audio playback aswell, if the datapack uses no audio, enabling this option will break the playback control")
 
     #parser.add_argument("-nv", "--no-video", help="doesn't convert the video to any format", action="store_true")
--- minecraft_movie_player_controller.py
+++ minecraft_movie_player_controller.py
@@ -1,6 +1,83 @@
+import os
+
+def fil(value):
+    if not os.path.isfile(value):
+        raise ValueError(f"the file '{value}' does not exist")
+    return value
+
+def fold(value):
+    if not os.path.isdir(value):
+        raise ValueError(f"the folder '{value}' dose not exist")
+    return value
+
+def palette(value):
+    pvalue = fil(value)
+    from image_converter import _get_palette
+    try:
+        _get_palette(pvalue)
+    except Exception:
+        raise ValueError(f"Could not read the file '{pvalue}', make sure it is formatted properly")
+    return pvalue
+
+def filename(value):
+    path = os.path.join(".", value)
+    if not os.path.exists(path):
+            try:
+                with open(path, "x") as _:
+                    pass
+                os.remove(path)
+            except OSError:
+                raise ValueError(f"The name '{value}' contains invalida characters")
+    return value
+
+def mfilename(max_chars):
+    def validate(value):
+        fvalue = filename(value)
+        if len(fvalue) > max_chars:
+            raise ValueError(f"the name {value} is {len(value)} characters long, should be at most {max_chars}")
+        return fvalue
+    return validate
+
+def mint(minimum, nullable: bool = False):
+    def validate(value):
+        if nullable and value is None:
+            return value
+        try:
+            ivalue = int(value)
+        except ValueError:
+            raise ValueError(f"Should be an integer above {minimum}, not {value}")
+        if ivalue <= minimum:
+            raise ValueError(f"Should be an integer above {minimum}, not {value}")
+        return ivalue
+    return validate
+
+def mfloat(minimum, nullable: bool = False):
+    def validate(value):
+        if nullable and value is None:
+            return value
+        try:
+            fvalue = float(value)
+        except ValueError:
+            raise ValueError(f"Should be a number above {minimum}, not {value}")
+        if fvalue <= minimum:
+            raise ValueError(f"Should be a number above {minimum}, not {value}")
+        return fvalue
+    return validate
+
+def vid(value):
+    fvalue = fil(value)
+    import cv2
+    video = cv2.VideoCapture(fvalue)
+    if not video.read()[0]:
+        video.release()
+        raise ValueError(f"the file '{fvalue}' is of an unknown format")
+    video.release()
+    return fvalue
 
 def video(path_to_video: str, path_to_output_folder: str, path_to_palette: str, name_prefix: str, ticks_per_frame: str, width: int, height: int, adjust_mode: str, unoptimized: bool, subprocesses: int):
     import image_converter as ic
+    if width is None and height is None:
+        width = 75
     ic.video_to_structure(path_to_video, path_to_output_folder, name_prefix, path_to_palette, ticks_per_frame, 0, width, height, adjust_mode, not unoptimized, subprocesses)
 
 def resourcepack_audio(path_to_audio: str, path_to_output_folder: str, split_size: float, name_prefix: str):
