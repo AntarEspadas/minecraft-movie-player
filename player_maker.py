09275390a357c428e3ede24a9b4e38196bc5eccd
Naratna <59068049+Naratna@users.noreply.github.com>
2020-09-28 09:10:03 -0500

    Resources folder is now zipped

--- player_maker.py
+++ player_maker.py
@@ -23,6 +23,7 @@ def make(containing_folder: str, resourcepack_subfolder: str = None, datapack_na
 
     shutil.make_archive(os.path.join(containing_folder,"resources"), "zip", os.path.join(containing_folder, "resources"))
 
+
 def _get_file_lists(containing_folder: str):
     files = [f for f in os.listdir(containing_folder) if os.path.isfile(os.path.join(containing_folder, f))]
     
