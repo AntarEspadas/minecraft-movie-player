3d303b35f61daf7456c4e779e00120ba28adc278
Naratna <59068049+Naratna@users.noreply.github.com>
2020-09-28 01:19:37 -0500

    If the audio file has more than two channels, downmixes it into stereo

--- player_resourcepack.py
+++ player_resourcepack.py
@@ -15,10 +15,11 @@ def split_and_convert(input_file: str, output_folder: str, name_prefix: str, seg
 
 
     audio = pydub.AudioSegment.from_file(input_file)
+    parameters = ["-ac", "2"] if audio.channels > 2 else []
     i = 0
     while i * segment_duration < audio.duration_seconds:
         segment = audio[i * segment_duration * 1000: (i + 1) * segment_duration * 1000]
-        file_handle = segment.export(os.path.join(output_folder, name_prefix + str(i) + ".ogg"), "ogg", "libvorbis")
+        file_handle = segment.export(os.path.join(output_folder, name_prefix + str(i) + ".ogg"), "ogg", "libvorbis", parameters=parameters)
         file_handle.close()
         i += 1
     return i
