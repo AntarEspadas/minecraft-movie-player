56884dfb196cfd38d94b07aab223100115a7ba14
Naratna <59068049+Naratna@users.noreply.github.com>
2020-10-01 15:32:28 -0500

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+HERE = os.path.abspath(".")
+
+with open("readme.md", "r") as readme:
+    long_description = readme.read()
+    
+install_requires ='''kdtree>=0.16
+nbtlib>=1.8.1
+opencv-python>=4.4.0.42
+pydub>=0.24.1'''
+
+setup(
+    name="minecraft-movie-player",
+    version = "",
+    author = "Naratna",
+    author_email = "antar.espadas@hotmail.com",
+    description = "A command line program that generates a datapack that plays videos in vanilla Minecraft",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Naratna/minecraft-movie-player",
+    packages = find_packages(),
+    classifiers = [
+        "programming Language :: Python :: 3",
+        "License:: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires = ">=3.8",
+    install_requires = install_requires,
+    entry_points='''[console_scripts]
+            mc-movie = minecraft_movie_player.__main__:main''',
+    license = "MIT",
+    includa_package_data=True,
+    package_data = {"": ["palette.txt", "index.txt"]}
+)
