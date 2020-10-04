5fe3add8f00a479c447ac040e13edc45d94f0e7c
Naratna <59068049+Naratna@users.noreply.github.com>
2020-10-04 18:12:39 -0500

    Changed csv format for palette and index to Excel flavored csv

--- minecraft_movie_player/palette_generator.py
+++ minecraft_movie_player/palette_generator.py
@@ -12,11 +12,11 @@ def __get_average_color(image: numpy.ndarray) -> list:
     return numpy.flip(avg_color)
 
 def __generate_palette_from_index(source_directory: str, destination_file: str, index_file: str):
-    palette_file = open(destination_file, "w+")
-    if index_file is not None:
-        index = open(index_file,newline="")
+    with open(destination_file, "w+", newline="") as palette_file:
+        writer = csv.writer(palette_file, delimiter=";", quotechar='"')
+        index = open(index_file, newline="")
         i = 0
-        for row in csv.reader(index,delimiter=",",quotechar="|"):
+        for row in csv.reader(index, delimiter=";", quotechar='"'):
             i += 1
             if len(row) < 2:
                 print(f"Not enough values in row {i}")
@@ -26,38 +26,37 @@ def __generate_palette_from_index(source_directory: str, destination_file: str, 
             image = cv2.imread(os.path.join(source_directory,row[0]))
             if image is not None:
                 color = __get_average_color(image)
-                palette_file.write(f"{row[1]},%d,%d,%d,|{row[2]}|\n" % tuple(color))
+                writer.writerow(row[1:2] + list(color) + row[2:])
             else:
                 print(f"Unable to read {row[0]}. File may not exist or not be an image")
         index.close()
-        palette_file.close()
 
 def generate_palette(source_directory: str, destination_file: str, index_file: str = None):
     if index_file is not None:
         __generate_palette_from_index(source_directory,destination_file,index_file)
         return
 
-    palette_file = open(destination_file, "w+")
-    files = __get_all_files(source_directory)
-    for file in files:
-        image = cv2.imread(os.path.join(source_directory,file))
-        if image is not None:
-            try:
-                color = __get_average_color(image)
-                palette_file.write(f"{file},%d,%d,%d\n" % tuple(color)) 
-            except Exception:
-                print(f"An unknown error ocurred reading file {file}")
-        else:
-            print(f"Unable to read {file}. File may not exist or not be an image")
-    palette_file.close()
+    with open(destination_file, "w+", newline="") as palette_file:
+        writer = csv.writer(palette_file, delimiter=";", quotechar='"')
+        files = __get_all_files(source_directory)
+        for file in files:
+            image = cv2.imread(os.path.join(source_directory,file))
+            if image is not None:
+                try:
+                    color = __get_average_color(image)
+                    writer.writerow([file] + list(color)) 
+                except Exception:
+                    print(f"An unknown error ocurred reading file {file}")
+            else:
+                print(f"Unable to read {file}. File may not exist or not be an image")
 
 def generate_index_template(source_directory: str, destination_file: str, filename_is_id: bool = True):
-    file = open(destination_file,"w+")
-    for image in __get_all_files(source_directory):
-        filename, extension = os.path.splitext(image)
-        if extension[1:].lower() == "png":
-            file.write(f"{image},{filename if filename_is_id else ''},\n")
-    file.close()
+    with open(destination_file,"w+", newline="") as f:
+        writer = csv.writer(f, delimiter=";", quotechar='"')
+        for image in __get_all_files(source_directory):
+            filename, extension = os.path.splitext(image)
+            if extension[1:].lower() == "png":
+                writer.writerow((image, filename if filename_is_id else "", ""))
 
 def main():
     import argparse
